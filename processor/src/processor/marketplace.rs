0df5d2b7338d1b97af63f531049cbc815a10fcab
SnB <skullandbonesdao@gmail.com>
2025-07-03 18:52:18 +0200

    init2

--- processor/src/processor/marketplace.rs
+++ processor/src/processor/marketplace.rs
@@ -1,6 +1,6 @@
 use decoder::staratlas::marketplace::{DecodedInstruction, ProcessExchange};
 use solana_sdk::pubkey::Pubkey;
-use solana_transaction_status::UiInstruction;
+use solana_transaction_status::{UiInstruction, UiParsedInstruction};
 
 pub struct MarketplaceProcessor {}
 
@@ -13,11 +13,14 @@ impl MarketplaceProcessor {
     ) {
         match decoder::staratlas::marketplace::decode_instruction(data.as_slice()) {
             Some(DecodedInstruction::ProcessExchange(exchange)) => {
-                for inner in inner_instructions {
+                for inner in inner_instructions.clone() {
                     match inner {
-                        UiInstruction::Parsed(parsed) => {
-                            println!("{:?}", parsed)
-                        }
+                        UiInstruction::Parsed(parsed) => match parsed {
+                            UiParsedInstruction::Parsed(parsed) => {
+                                println!("program_id={:?}", parsed.program_id);
+                            }
+                            _ => panic!("Unhandled UiParsedInstruction type"),
+                        },
                         _ => panic!("Unhandled UiInstruction type"),
                     }
                 }
@@ -36,6 +39,21 @@ impl MarketplaceProcessor {
                 signature,
                 hex::encode(data)
             ),
+        }
+    }
+
+    fn map_inner_transfers(inner_instructions: Vec<UiInstruction>) {
+        let mut mapped_inner = vec![];
+        for inner in inner_instructions.clone() {
+            match inner {
+                UiInstruction::Parsed(parsed) => match parsed {
+                    UiParsedInstruction::Parsed(parsed) => {
+                        mapped_inner.push(parsed.program_id);
+                    }
+                    _ => panic!("Unhandled UiParsedInstruction type"),
+                },
+                _ => panic!("Unhandled UiInstruction type"),
+            }
         }
     }
 }
